d8ff7edb4c7cd58b28d585f4436d09b9d195e14b
Yakup Kadri Demirci <31820045+yakupdemircia@users.noreply.github.com>
2020-10-23 10:53:53 +0300

    Update Rx.c
    comments translated to English

--- Software/Rx/Rx.c
+++ Software/Rx/Rx.c
@@ -1,20 +1,20 @@
 /* RANGE Rx V2.0
-  PIC18F46K20, 16 MHz, WDT 1:64, BrownOut, int0, usart interrupt, opsiyonel lcd */
+  PIC18F46K20, 16 MHz, WDT 1:64, BrownOut, int0, usart interrupt, optional lcd */
 
-#define yukari  PORTC.B4=1
-#define asagi   PORTD.B3=1
-#define sag     PORTD.B0=1
-#define sol     PORTC.B3=1
-#define ileri   PORTC.B0=1
-#define geri    PORTA.B6=1
-#define yukari2 PORTD.B2=1
-#define asagi2  PORTD.B1=1
-#define sag2    PORTC.B2=1
-#define sol2    PORTC.B1=1
-#define ileri2  PORTA.B7=1
-#define geri2   PORTE.B2=1
-#define acil    PORTC.B5=1
-#define siren   PORTE.B1=1
+#define up  PORTC.B4=1
+#define down   PORTD.B3=1
+#define right     PORTD.B0=1
+#define left     PORTC.B3=1
+#define fwd   PORTC.B0=1
+#define back    PORTA.B6=1
+#define up2 PORTD.B2=1
+#define down2  PORTD.B1=1
+#define right2    PORTC.B2=1
+#define left2    PORTC.B1=1
+#define fwd2  PORTA.B7=1
+#define back2   PORTE.B2=1
+#define emergency    PORTC.B5=1
+#define syren   PORTE.B1=1
 #define setA    PORTD.B4
 #define setB    PORTD.B5
 #define aux     PORTB.B0
@@ -22,43 +22,43 @@
 unsigned char data1;
 unsigned char buff;
 unsigned char RXBUFF[7] = {'0','0','0','0','0','0','0'};
-unsigned char paketSay = 0;
+unsigned char packageCounter = 0;
 unsigned char ID1 = 117;
 unsigned char ID2 = 45;
 unsigned char ID3 = 46;
 unsigned char ID4 = 45;
-unsigned char veriOk = 0;
-unsigned int tmrSay = 0;
-unsigned char dSay = 0;
+unsigned char dataOk = 0;
+unsigned int tmrCounter = 0;
+unsigned char dCount = 0;
 unsigned char xdata=0;
 
 
-void hataTemizle()
+void cleanErrors()
 {
 
-    if (RCSTA.OERR) //Overrun Hatasý oluþtuysa
+    if (RCSTA.OERR) //Overrun Error
     {
             TXSTA.TXEN=0;
             TXSTA.TXEN=1;
             RCSTA.CREN=0;
             RCSTA.CREN=1;
     }
-    if (RCSTA.FERR) //Frame hatasý oluþtuysa
+    if (RCSTA.FERR) //Frame Error
     {
             buff=RCREG;
             TXSTA.TXEN=0;
             TXSTA.TXEN=1;
     }
 }
-void rs232KarakterGonder()
+void sendRs232Character()
 {
     while(!PIR1.TXIF)
     {
-        hataTemizle();
+        cleanErrors();
     }
     TXREG = data1;
 }
-  void cikisIsle(unsigned char sy, unsigned char p)
+  void processOutput(unsigned char sy, unsigned char p)
   {
           if(p == 0)
           {
@@ -66,38 +66,38 @@ void rs232KarakterGonder()
                   {
                           case 1:
                           {
-                                  geri;
-                                  siren;
+                                  back;
+                                  syren;
                                   break;
                           }
                           case 2:
                           {
-                                  ileri;
-                                  siren;
+                                  fwd;
+                                  syren;
                                   break;
                           }
                           case 4:
                           {
-                                  sol;
-                                  siren;
+                                  left;
+                                  syren;
                                   break;
                           }
                           case 8:
                           {
-                                  sag;
-                                  siren;
+                                  right;
+                                  syren;
                                   break;
                           }
                           case 16:
                           {
-                                  asagi;
-                                  siren;
+                                  down;
+                                  syren;
                                   break;
                           }
                           case 32:
                           {
-                                  yukari;
-                                  siren;
+                                  up;
+                                  syren;
                                   break;
                           }
                           default:
@@ -112,44 +112,44 @@ void rs232KarakterGonder()
                   {
                           case 1:
                           {
-                                  geri;
-                                  geri2;
-                                  siren;
+                                  back;
+                                  back2;
+                                  syren;
                                   break;
                           }
                           case 2:
                           {
-                                  ileri;
-                                  ileri2;
-                                  siren;
+                                  fwd;
+                                  fwd2;
+                                  syren;
                                   break;
                           }
                           case 4:
                           {
-                                  sol;
-                                  sol2;
-                                  siren;
+                                  left;
+                                  left2;
+                                  syren;
                                   break;
                           }
                           case 8:
                           {
-                                  sag;
-                                  sag2;
-                                  siren;
+                                  right;
+                                  right2;
+                                  syren;
                                   break;
                           }
                           case 16:
                           {
-                                  asagi;
-                                  asagi2;
-                                  siren;
+                                  down;
+                                  down2;
+                                  syren;
                                   break;
                           }
                           case 32:
                           {
-                                  yukari;
-                                  yukari2;
-                                  siren;
+                                  up;
+                                  up2;
+                                  syren;
                                   break;
                           }
                           default:
@@ -159,7 +159,7 @@ void rs232KarakterGonder()
                   }
           }
   }
-void cikisTemizle()
+void cleanOutput()
 {
         PORTA = 0;
         PORTB = 0;
@@ -167,35 +167,35 @@ void cikisTemizle()
         PORTD = 0;
         PORTE = 0;
 }
-void cikisKontrol()
+void outputControl()
 {
-       cikisTemizle();
+       cleanOutput();
 
         if(((RXBUFF[5]&64)>0)&&(RXBUFF[5]<255))
         {
-                acil;
-                siren;
+                emergency;
+                syren;
         }
         else if(RXBUFF[5]==255)
         {
-        cikisTemizle();
+        cleanOutput();
         }
         else
         {
-                for(dSay = 1; dSay<128; dSay = dSay*2)
+                for(dCount = 1; dCount<128; dCount = dCount*2)
                 {
-                        if((RXBUFF[5]&dSay)>0)
+                        if((RXBUFF[5]&dCount)>0)
                         {
                                 xdata=0;
-                                cikisIsle(dSay,xdata);
+                                processOutput(dCount,xdata);
                         }
                 }
-                for(dSay = 1; dSay<64; dSay = dSay*2)
+                for(dCount = 1; dCount<64; dCount = dCount*2)
                 {
-                        if((RXBUFF[6]&dSay)>0)
+                        if((RXBUFF[6]&dCount)>0)
                         {
                                 xdata=1;
-                                cikisIsle(dSay,xdata);
+                                processOutput(dCount,xdata);
                         }
                 }
         }
@@ -204,25 +204,25 @@ void interrupt()
 {
        if(PIR1.RCIF)
         {
-                RXBUFF[paketSay] = RCREG;
-                if(RXBUFF[paketSay]==85)      //Senkronizasyon
+                RXBUFF[packageCounter] = RCREG;
+                if(RXBUFF[packageCounter]==85)      //Sync
                 {
-                  if(paketSay>0)
+                  if(packageCounter>0)
                   {
-                  paketSay=0;
+                  packageCounter=0;
                   RXBUFF[0]=85;
                   }
                 }
-                paketSay++; 
+                packageCounter++; 
 
-                if(paketSay == 7)
+                if(packageCounter == 7)
                 {
-                        paketSay = 0;
+                        packageCounter = 0;
                         if(RXBUFF[0] == 0x55 && RXBUFF[1] == ID1 && RXBUFF[2] == ID2 && RXBUFF[3] == ID3 && RXBUFF[4] == 0x11)
                         {
-                                veriOk = 1;
-                                tmrSay=0;
-                                cikisKontrol();
+                                dataOk = 1;
+                                tmrCounter=0;
+                                outputControl();
                         }
                 }
                 PIR1.RCIF = 0;
@@ -230,7 +230,7 @@ void interrupt()
 
 }
 
-void Ayarlar()
+void Settings()
 { 
 ANSEL=0;
 ANSELH=0;
@@ -238,16 +238,16 @@ TRISC.B4=0; TRISD.B3=0; TRISD.B2=0; TRISD.B1=0; TRISD.B0=0; TRISC.B3=0; TRISC.B2
 TRISA.B7=0; TRISE.B2=0; TRISC.B5=0; TRISE.B1=0; TRISD.B4=0; TRISD.B5=0;
 PORTA=0; PORTB=0; PORTC=0; PORTD=0; PORTE=0;
 
-        INTCON.PEIE=1;  //Peripheral interrupt açýk
-        PIE1.RCIE=1;   //Usart receive kesmesi açýk        
-        INTCON.GIE=1;  //Global kesmeler açýk
-        RCON.IPEN=1;  //Ýnterrupt öncelikleri açýk
-        ADCON0.ADON=0; //ADC modülü tamamen kapatýldý
+        INTCON.PEIE=1;  //Peripheral interrupt enabled
+        PIE1.RCIE=1;   //Usart receive interrupt enabled     
+        INTCON.GIE=1;  //Global interrupts enabled
+        RCON.IPEN=1;  //Interrupt priorities enabled
+        ADCON0.ADON=0; //ADC module disabled
         CM1CON0.C1ON=0;
-        CM2CON0.C2ON=0; //Comparatorler kapatýldý
-        T2CON = 0b011111100;              //bu biraz sýkýntýlý
+        CM2CON0.C2ON=0; //Comparators disabled
+        T2CON = 0b011111100;   
         TMR2 = 0;
-        PR2 = 0xFF; //tmr2 periyot
+        PR2 = 0xFF; //tmr2 period
         UART1_Init(9600);
         //Lcd_Init();
         Delay_ms(100);
@@ -256,28 +256,28 @@ PORTA=0; PORTB=0; PORTC=0; PORTD=0; PORTE=0;
 }
 void main() 
 {
-        Ayarlar();
+        Settings();
          OSCCON=0b01010111;
          OSCTUNE=0b10011111;
 
        while(1)
         {
 
-                        tmrSay++;
-                        if(tmrSay == 20000)
+                        tmrCounter++;
+                        if(tmrCounter == 20000)
                         {
                                 tmr2=0;
-                                tmrSay = 0;
-                                if(veriOk == 0)
+                                tmrCounter = 0;
+                                if(dataOk == 0)
                                 {       
                                    if(RXBUFF[5]!=64)
                                    {
-                                        paketSay = 0; // dikkat sorun cikabilir
-                                        cikisTemizle();
+                                        packageCounter = 0;
+                                        cleanOutput();
                                    }
                                 }
                                 else
-                                {veriOk = 0;}
+                                {dataOk = 0;}
                         }
                 asm CLRWDT;
         }
